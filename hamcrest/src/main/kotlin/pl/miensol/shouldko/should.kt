ecbbfdce17e78b690cdcdd82ad76e497b2821ea6
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2017-11-02 19:35:26 +0100

    More tests

--- hamcrest/src/main/kotlin/pl/miensol/shouldko/should.kt
+++ hamcrest/src/main/kotlin/pl/miensol/shouldko/should.kt
@@ -5,10 +5,24 @@ import org.hamcrest.MatcherAssert
 import org.hamcrest.Matchers
 import pl.miensol.shouldko.internal.prependTopStackFrameSourceLine
 
-fun <T> T.shouldBe(matcher: Matcher<T>): T = apply {
+fun <T> T.should(matcher: Matcher<T>): T = apply {
     prependTopStackFrameSourceLine {
         MatcherAssert.assertThat(this, matcher)
     }
 }
 
-fun <T> T.shouldNotBe(matcher: Matcher<T>): T = shouldBe(Matchers.not(matcher))
+fun <T> T.shouldNot(matcher: Matcher<T>): T = should(Matchers.not(matcher))
+
+fun <T> T.shouldEqual(expected: T): T = should(Matchers.equalTo(expected))
+
+fun <T> T.shouldNotEqual(expected: T): T = shouldNot(Matchers.equalTo(expected))
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Iterable<T>.shouldContain(item: T): Iterable<T> = should(Matchers.hasItem(item) as Matcher<Iterable<T>>)
+
+@Suppress("UNCHECKED_CAST")
+fun <T> Iterable<T>.shouldNotContain(item: T): Iterable<T> = shouldNot(Matchers.hasItem(item) as Matcher<Iterable<T>>)
+
+fun String.shouldContain(substring: String): String = should(Matchers.containsString(substring))
+
+fun String.shouldNotContain(substring: String): String = shouldNot(Matchers.containsString(substring))
--- hamcrest/src/test/kotlin/tests/pl/miensol/shouldko/hamcrest/AssertionTests.kt
+++ hamcrest/src/test/kotlin/tests/pl/miensol/shouldko/hamcrest/AssertionTests.kt
@@ -5,40 +5,109 @@ import org.hamcrest.Matchers.containsString
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import pl.miensol.shouldko.shouldBe
-import pl.miensol.shouldko.shouldNotBe
+import pl.miensol.shouldko.*
 
 class AssertionTests {
     @Test
-    fun `single variable equals`() {
+    fun `single variable equal`() {
         val myVariable = "some value"
 
         val error = Assertions.assertThrows(AssertionError::class.java, {
-            myVariable.shouldBe(equalTo("some other value"))
+            myVariable.should(equalTo("some other value"))
         })
 
         assertThat(error.message, containsString("myVariable"))
     }
 
     @Test
-    fun `single variable not equals`() {
+    fun `single variable not equal`() {
         val myVariable = "some value"
 
         val error = Assertions.assertThrows(AssertionError::class.java, {
-            myVariable.shouldNotBe(equalTo(myVariable))
+            myVariable.shouldNot(equalTo(myVariable))
         })
 
         assertThat(error.message, containsString("myVariable"))
     }
 
     @Test
-    fun `method invocation equals`() {
+    fun `method invocation equal`() {
         fun myFunction() = "some value"
 
         val error = Assertions.assertThrows(AssertionError::class.java) {
-            myFunction().shouldBe(equalTo("some other value"))
+            myFunction().should(equalTo("some other value"))
         }
 
         assertThat(error.message, containsString("myFunction()"))
     }
+
+    @Test
+    fun `expression equal`() {
+        val a = 1
+
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            (a + 2).shouldEqual(4)
+        }
+
+        assertThat(error.message, containsString("a + 2"))
+    }
+
+    @Test
+    fun `lambda expression not equal to`() {
+        val a = { 1 + 1 }
+
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            (a() + 1).shouldNotEqual(3)
+        }
+
+        assertThat(error.message, containsString("a() + 1"))
+    }
+
+    @Test
+    fun `inline functions equal to`() {
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            sum(1, 2).shouldEqual(4)
+        }
+
+        assertThat(error.message, containsString("sum(1, 2)"))
+    }
+
+    @Test
+    fun `collection contain item`() {
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            listOf(1, 2, 3).shouldContain(4L)
+        }
+
+        assertThat(error.message, containsString("listOf(1, 2, 3)"))
+    }
+
+    @Test
+    fun `collection not contain item`() {
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            listOf(1, 2, 3).shouldNotContain(3)
+        }
+
+        assertThat(error.message, containsString("listOf(1, 2, 3)"))
+    }
+
+    @Test
+    fun `string contains substring`() {
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            ("ala" + "ma kota").shouldContain("makota")
+        }
+
+        assertThat(error.message, containsString("""("ala" + "ma kota")"""))
+    }
+
+    @Test
+    fun `string does not contain substring`() {
+        val error = Assertions.assertThrows(AssertionError::class.java) {
+            ("ala" + "ma kota").shouldNotContain("ma")
+        }
+
+        assertThat(error.message, containsString("""("ala" + "ma kota")"""))
+    }
+
+    @Suppress("NOTHING_TO_INLINE")
+    inline fun sum(a: Int, b: Int) = a + b
 }
